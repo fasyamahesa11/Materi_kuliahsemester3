24fe94fcff0a5f01b436f19e18ff23e54e1e56b6
asa <fasyamahesa011@gmail.com>
2024-09-12 12:01:44 +0700

    pertemuan1 PBO

---
+++ PBO/fungsionall.py
@@ -0,0 +1,6 @@
+# membuat program paradugma fungsional
+fungsi_persegipanjang = lambda panjang, lebar: panjang * lebar
+panjang = 5 
+lebar = 10
+
+print(f"luas persegi panjang {fungsi_persegipanjang(panjang,lebar)}")
---
+++ PBO/prosuderal.py
@@ -0,0 +1,6 @@
+# Membuat Program Paradigma Prosuderal
+def luas_persegi(panjang, lebar):
+    return panjang * lebar
+
+luas = luas_persegi(10, 5)
+print (luas)
