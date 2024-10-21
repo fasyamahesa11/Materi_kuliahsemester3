e0de37c7cf24d3a599f89119b4deb5f29be1565e
asa <fasyamahesa011@gmail.com>
2024-10-21 11:45:02 +0700

    pertemuan6

---
+++ pem_web/pertemuan6/latihan2.js
@@ -0,0 +1,27 @@
+// Arithmatic Oprators
+let sum = 10 + 5; //addition
+let difference = 10 - 5; //subtraction
+let product = 10 * 5; //multiplication
+let quotient = 10 / 5; //division
+let remainder = 10 % 5; //modulo
+let exponent = 10 ** 5; //exponent
+console.log(sum, "\n", difference, product, quotient, remainder, exponent);
+
+// Comparison Operators
+let isEqual = 5 === "5"; //strict equality sm dg 3
+let isEqual2 = 5 == "5"; //not strict sm dg ada 2
+let isNotEqual = 10 == "10"; //sama dengan asa 1 strick
+let isNotEqual2 = 10 === "10"; //sama dengan ada 2 not strick
+let isGreaterThan = 15 > 10;
+console.log(isEqual, isEqual2, isNotEqual, isNotEqual2, isGreaterThan);
+
+//Logical Operators
+let hours = 11;
+let isBothTrue = true && true;
+let isBothTrue2 = true && false;
+let isEitherTrue = true || false;
+let isEitherTrue2 = true || true;
+let isEitherTrue3 = false || false;
+let isNotTrue = !true;
+let isNotFalse = !false;
+console.log(isBothTrue, isBothTrue2, isEitherTrue, isEitherTrue2, isEitherTrue3, isNotTrue, isNotFalse);
