856dbb8deb1b7957f88952e1be0355cbe6ee0dc9
asa <fasyamahesa011@gmail.com>
2024-11-25 11:37:11 +0700

    pertemuan10

---
+++ pem_web/pertemuan10/main.js
@@ -0,0 +1,8 @@
+let msg = document.getElementById("my-button");
+msg.addEventListener("click", () => {
+    alert("button clicked");
+});
+let btn = document.getElementById("mouse-over");
+btn.addEventListener("mouseover", () =>{
+    alert("mouse over");
+});
