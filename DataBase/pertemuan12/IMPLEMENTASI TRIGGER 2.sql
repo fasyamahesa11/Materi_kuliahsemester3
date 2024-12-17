dd6ef6343226e359959d40eba3282eefb1233b95
asa <fasyamahesa011@gmail.com>
2024-12-17 11:22:32 +0700

    PBO

---
+++ DataBase/pertemuan12/IMPLEMENTASI TRIGGER 2.sql
@@ -0,0 +1,3 @@
+# implementasi trigger after inserrt pengadaan
+INSERT INTO tbl_detail_pengadaan VALUES (
+"PGD_002", "BRG_003", 10, 1000000);
---
+++ DataBase/pertemuan12/implementasi trigger 1.sql
@@ -0,0 +1,3 @@
+SET foreign_key_cheks = 0;
+DELETE FROM tbl_barang WHERE kode_barang = 'BRG_001';
+SELECT * FROM tbl_arsip_barang;
